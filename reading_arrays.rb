4fab5f58ec2767e30e0e0cdcd75c3bea9ee2ce2c
twinitiee <twinitiee@gmail.com>
2018-07-25 12:58:01 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -30,3 +30,10 @@ end
 def first_second_and_third_students
   puts STUDENT_NAMES[1,2,3]
 end
+
+first_student_by_index
+fourth_student_by_index
+last_student_by_index
+first_student_by_method
+last_student_by_method
+first_second_and_third_students
